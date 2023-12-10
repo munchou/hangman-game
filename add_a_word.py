fb74bccb80f54c038f152d1a999170e7535bbb0f
munchou <99625882+munchou@users.noreply.github.com>
2023-12-10 22:22:43 +0900

    Feature to import words from definitions_retriever/downloaded_definitions.json

--- add_a_word.py
+++ add_a_word.py
@@ -2,6 +2,7 @@ import json
 
 
 words_file = "words.json"
+imported_words_file = "definitions_retriever/downloaded_definitions.json"
 alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 
@@ -18,9 +19,10 @@ def title():
 def language_select():
     title()
     while True:
-        available_choice = ["1", "2", "123"]
+        available_choice = ["1", "2", "3", "123"]
         print("\n\t1. ENGLISH")
         print("\t2. FRANCAIS")
+        print("\t3. IMPORT ENGLISH WORDS FROM THE 'DOWNLOADED DEFINITIONS' JSON FILE")
         print("\t123. EXIT")
         choice = input("\t-----> ")
 
@@ -29,14 +31,19 @@ def language_select():
         else:
             if choice == "1":
                 language = "english"
+                special = "ok"
             elif choice == "2":
                 language = "francais"
+                special = "ok"
+            elif choice == "3":
+                language = "english"
+                special = "special"
             elif choice == "123":
                 exit()
-            return language
+            return language, special
 
 
-language = language_select()
+language, special = language_select()
 
 
 def loadWords():
@@ -51,6 +58,26 @@ words_keys = []
 for i in words_list:
     for u in i:
         words_keys.append(u)
+
+if special == "special":
+    while True:
+        choice = input(
+            'Are you sure you wish to import words from "definitions_retriever/downloaded_definitions.json"? (y/n)'
+        ).casefold()
+        if choice == "y":
+            break
+        elif choice == "n":
+            exit()
+        continue
+    print(words_keys)
+    with open(imported_words_file, encoding="utf8") as file:
+        words_to_import = json.load(file)
+
+    new_words = []
+    for word in words_to_import:
+        if word not in words_keys:
+            new_word = {word: words_to_import[word]}
+            words_list.append(new_word)
 
 
 def add_word_and_def():
@@ -83,8 +110,9 @@ def add_word_and_def():
     return {word: definition}
 
 
-new_word = add_word_and_def()
-words_list.append(new_word)
+if special == "ok":
+    new_word = add_word_and_def()
+    words_list.append(new_word)
 
 with open(words_file, "w", encoding="utf8") as json_file:
     """Write the new data to the file"""
